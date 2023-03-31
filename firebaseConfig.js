f3cb1af1e62c9ff32b193a632e1cc98b064e55f7
LightningItself <68198395+LightningItself@users.noreply.github.com>
2023-03-31 15:23:18 +0530

    minor changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,16 +3,17 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvS8O09dk9ciDCyCD0hD99I3fwGyudTFw",
-  authDomain: "gc-mess-manager.firebaseapp.com",
-  projectId: "gc-mess-manager",
-  storageBucket: "gc-mess-manager.appspot.com",
-  messagingSenderId: "645303666804",
-  appId: "1:645303666804:web:b888abe7764b70b5ed6921",
-  measurementId: "G-PW5NX6L99M",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-export { app };
+export { app, db };
