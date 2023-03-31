a1fb42acdb2175ec017d56380124799c89364764
Ayush pratap singh <ayushpratap9528@gmail.com>
2023-04-01 03:13:07 +0530

    Login page withou google auth

--- MessManager/src/views/Login.jsx
+++ MessManager/src/views/Login.jsx
@@ -1,21 +1,152 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  Image,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { Button } from "@rneui/themed";
 import { ThemeProvider } from "react-native-paper";
-
+import MatwerialICon from "react-native-vector-icons/MaterialIcons";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { cloneElement } from "react";
 const Login = ({ navigation }) => {
   return (
-    <View>
-      <Button
-        buttonStyle={{ marginHorizontal: 50, marginTop: 50 }}
-        onPress={() => navigation.navigate("User")}
-        title="Student Login"
-      />
-      <Button
-        buttonStyle={{ margin: 50 }}
-        onPress={() => navigation.navigate("Admin")}
-        title="Server Login"
-      />
-    </View>
+    // <View>
+    //
+    //   <Button
+    //     buttonStyle={{ marginHorizontal: 50, marginTop: 50 }}
+    //     onPress={() => navigation.navigate("User")}
+    //     title="Student Login"
+    //   />
+    //   <Button
+    //     buttonStyle={{ margin: 50 }}
+    //     onPress={() => navigation.navigate("Admin")}
+    //     title="Server Login"
+    //   />
+    // </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
+      <View style={{ paddingHorizontal: 20 }}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../assets/lunch.png")}
+            style={{ height: 200, width: 200 }}
+          />
+        </View>
+        <Text style={{ fontSize: 30, paddingBottom: 30 }}>Login</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <MatwerialICon
+            name="alternate-email"
+            size={20}
+            color="#666"
+            style={{ marginRight: 5 }}
+          ></MatwerialICon>
+          <TextInput
+            placeholder="Email-ID"
+            style={{ flex: 1, paddingVertical: 0 }}
+            keyboardType="email-address"
+          ></TextInput>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <Ionicons
+            name="ios-lock-closed-outline"
+            size={20}
+            color="#666"
+            style={{ marginRight: 5 }}
+          />
+          <TextInput
+            placeholder="Password"
+            style={{ flex: 1, paddingVertical: 0 }}
+            secureTextEntry={true}
+          ></TextInput>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={{ color: "#AD40AF", fontWeight: 700 }}>
+              Forgot password
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("User")}
+            style={{
+              backgroundColor: "#AD40AF",
+              padding: 20,
+              borderRadius: 10,
+              marginBottom: 25,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontWeight: 700,
+                fontSize: 16,
+                color: "#ffff",
+              }}
+            >
+              LOGIN
+            </Text>
+          </TouchableOpacity>
+          <Text
+            style={{ textAlign: "center", color: "#666", marginBottom: 20 }}
+          >
+            {" "}
+            Or login with...
+          </Text>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <TouchableOpacity
+               onPress={() => {}}
+              style={{
+                borderColor: "#ddd",
+                borderWidth: 2,
+                borderRadius: 10,
+                paddingHorizontal: 100,
+                paddingVertical: 10,
+              }}
+            >
+              <Image
+                source={{
+                  url: "https://static.vecteezy.com/system/resources/previews/009/469/630/original/google-logo-isolated-editorial-icon-free-vector.jpg",
+                }}
+                style={{ height: 24, width: 24 }}
+              ></Image>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignContent: "flex-end",
+            marginBottom: 30,
+            marginTop: 30,
+          }}
+        >
+          <Text>Admin </Text>
+          <TouchableOpacity  onPress={() => navigation.navigate("Admin")}>
+            <Text style={{ color: "#AD40AF", fontWeight: 700 }}>
+              Click Here
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
