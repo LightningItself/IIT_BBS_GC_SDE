cdb8eaffa943edbbc3a89f03e3eec41f69c48949
LightningItself <68198395+LightningItself@users.noreply.github.com>
2023-04-01 19:16:52 +0530

    user side backend backend working

---
+++ MessManager/firebaseConfig.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBvS8O09dk9ciDCyCD0hD99I3fwGyudTFw",
+  authDomain: "gc-mess-manager.firebaseapp.com",
+  databaseURL:
+    "https://gc-mess-manager-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gc-mess-manager",
+  storageBucket: "gc-mess-manager.appspot.com",
+  messagingSenderId: "645303666804",
+  appId: "1:645303666804:web:b888abe7764b70b5ed6921",
+  measurementId: "G-PW5NX6L99M",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { app, db };
